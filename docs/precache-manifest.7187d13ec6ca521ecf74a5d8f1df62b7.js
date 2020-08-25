71f78ba58b4cada1c7edc4a7823c55c1764c9f1b
Nelson <n.sanchezesparza@gmail.com>
2020-08-25 11:26:20 -0500

    subir docs

---
+++ docs/precache-manifest.7187d13ec6ca521ecf74a5d8f1df62b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c61a9dd541d485bdedc4201cc3d5111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6df91916f0072034e9",
+    "url": "/static/css/main.95b89998.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6df91916f0072034e9",
+    "url": "/static/js/main.4dae58b9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
